da6d1e78d1194bac0f48df1634444edcd00a7035
msjun23 <msjun23@gmail.com>
2022-06-19 21:12:24 +0900

    Update for jetson

--- scripts/joy_node.py
+++ scripts/joy_node.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
-from sensor_msgs.msg import Joy
+import cv2
+import os
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy, Image
 from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
+from actionlib_msgs.msg import GoalID
+
 
 '''
 Jetson AGX Xavier
@@ -42,9 +49,13 @@ class joy_node:
         rospy.init_node('joy_node', anonymous=True)
         
         rospy.Subscriber("/joy", Joy, self.JoyCallback)
+        rospy.Subscriber("/camera/color/image_raw", Image, self.Capture)
         self.pub_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        
+        self.mode_pub = rospy.Publisher('/umbot_mode', String, queue_size=10)
+        self.cancel_pub = rospy.Publisher('/move_base/cancel', GoalID, queue_size=1)
+
         self.cmd_vel = Twist()
+        self.path = os.getcwd()
         
         rospy.on_shutdown(self.QuitHandler)
         rospy.spin()
@@ -66,7 +77,7 @@ class joy_node:
                 
             self.pub_vel.publish(self.cmd_vel)
             rospy.loginfo('Running...')
-        elif (joy.buttons[0] == 1 or joy.buttons[1] == 1 or joy.buttons[3] == 1 or joy.buttons[4] == 1):    # Stop
+        elif (joy.buttons[0] == 1):    # Stop
             self.cmd_vel.linear.x = 0.0
             self.cmd_vel.linear.y = 0.0
             self.cmd_vel.linear.z = 0.0
@@ -74,9 +85,29 @@ class joy_node:
             self.cmd_vel.angular.x = 0.0
             self.cmd_vel.angular.y = 0.0
             self.cmd_vel.angular.z = 0.0
+            
+            cancel_msg = GoalID()
+            
             self.pub_vel.publish(self.cmd_vel)
+            self.cancel_pub.publish(cancel_msg)
+            self.mode_pub.publish('stop')
+        elif (joy.buttons[1] == 1):     # Cleaning mode
+            self.mode_pub.publish('cleaning')
+        elif (joy.buttons[3] == 1):     # Disinfection mode
+            self.mode_pub.publish('disinfection')
+        elif (joy.buttons[4] == 1):
+            rospy.loginfo('sibal')
+            # file_name = self.path + '/save/' + str(self.img.header.seq) + '.jpg'
+            # file_name = '~/datasets/capture/' + str(self.img.header.seq) + '.jpg'
+            # rospy.loginfo(file_name)
+            # cv2.imwrite(file_name, self.cv_img)
         else:                                                   # Without any key input, don't publish any command
             pass
+        
+    def Capture(self, data):
+        bridge = CvBridge()
+        self.img = data
+        self.cv_img = bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
             
     def QuitHandler(self):
         rospy.loginfo('Shuting down process...')
